8b225f3793319ab0ac8c12bd978e99cd6b87def5
mike-droid <miguelreyesmoreno@hotmail.com>
2023-06-21 10:48:27 -0600

    condicionales jinja

--- myapp/models.py
+++ myapp/models.py
@@ -13,6 +13,7 @@ class Task(models.Model):
   title = models.CharField(max_length=200)
   description = models.TextField()
   project = models.ForeignKey(Project, on_delete=models.CASCADE)
+  done = models.BooleanField(default=False)
 
   def __str__(self) -> str:
     return f'Task: {self.title} -> Project: {self.project.name}'
