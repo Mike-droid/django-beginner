a3c1f03a369029047b03a8a5e7bf68b623cad5ff
mike-droid <miguelreyesmoreno@hotmail.com>
2023-06-20 18:01:28 -0600

    django admin

--- myapp/models.py
+++ myapp/models.py
@@ -5,8 +5,14 @@ from django.db import models
 class Project(models.Model):
   name = models.CharField(max_length=200)
 
+  def __str__(self) -> str:
+    return self.name
+
 
 class Task(models.Model):
   title = models.CharField(max_length=200)
   description = models.TextField()
   project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+  def __str__(self) -> str:
+    return f'Task: {self.title} -> Project: {self.project.name}'
